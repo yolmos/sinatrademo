5edfccf93b6dc15ef664079a56f9f729cf59d392
Yuriev Olmos <yuriev@gmail.com>
2016-07-13 16:29:41 -0500

    Priratasrackup

--- app.rb
+++ app.rb
@@ -1,5 +1,20 @@
+# app.rb
 require 'sinatra'
 
-get '/' do
-  "Hello, world"
+class HelloWorldApp < Sinatra::Base
+	get '/' do
+
+"<!DOCTYPE html>
+<html>
+<head>
+  <title>Demo</title>
+</head>
+
+<body>
+  <h2>Abordar</h2>
+  <img src=\"http://www.alisoneldred.com/blog/wp-content/uploads/2014/05/Ancillary-Injustice-Complete-31.jpg\"/>
+</body>
+</html>
+"
+	end
 end
